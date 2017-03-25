ba28c80ee158c5b2d09133b508555e87e74806b3
Vincent Portelli <vinport16@gmail.com>
2017-03-25 08:50:28 -0400

    form send data

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var app = require('express')();
+var bodyparser = require('body-parser');
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var sio = require('socket.io');
 var io = sio(http);
@@ -6,9 +8,29 @@ var io = sio(http);
 var port = process.env.PORT || 8080;
 http.listen(port);
 
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyparser.json());
+
+app.post("/done", function (req, res) {
+    console.log(req.body)
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/form", function (req, res) {
+    res.sendFile(__dirname + '/form.html');
+});
+
+
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/form', function(req, res){
+  res.sendFile(__dirname + '/form.html');
 });
 
 
@@ -24,4 +46,3 @@ io.on("connection", function(socket){
 io.on('connection', function(socket){
   
 });
-
