0f7cf733b54d30a5527cf93251c0d264a06e6284
Vincent Portelli <vinport16@gmail.com>
2017-03-26 01:57:01 -0400

    fixed matching broke db

--- server.js
+++ server.js
@@ -55,11 +55,11 @@ app.post("/done", function (req, res) { //process form submission: req.body cont
 
       db.run("CREATE TABLE if not exists user_info (id INTEGER primary key, first TEXT, last TEXT, email TEXT, age INTEGER, state TEXT, candidate TEXT, cabort TEXT, abort INTEGER, cgov TEXT, gov INTEGER, cgun TEXT, gun INTEGER, cwarm TEXT, warm INTEGER, cwage TEXT, wage TEXT, cbord TEXT, bord INTEGER, clgbt TEXT, lgbt INTEGER, cedu TEXT, edu INTEGER, cadmin TEXT, admin INTEGER, paired INTEGER)");
       var stmt = db.prepare("INSERT INTO user_info (first, last, email, age, state, candidate, cabort, abort, cgov, gov, cgun, gun, cwarm, warm, cwage, wage, cbord, bord, clgbt, lgbt, cedu, edu, cadmin, admin, paired) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)");
-      stmt.run([first, last, email, age, state, candidate, cabort, abort, cgov, gov, cgun, gun, cwarm, warm, cwage, cbord, bord, clgbt, lgbt, cedu, edu, cadmin, admin, paired]);
+      stmt.run([first, last, email, age, state, candidate, cabort, abort, cgov, gov, cgun, gun, cwarm, warm, cwage, wage, cbord, bord, clgbt, lgbt, cedu, edu, cadmin, admin, paired]);
       stmt.finalize();
-      db.each ("SELECT * FROM user_info WHERE paired = 0", function (err, row){
-        console.log(row);
+      db.all("SELECT * FROM user_info WHERE paired = 0", function (err, row){
         var people = row;
+        pair(people);
       });
     });
     res.sendFile(__dirname + '/index.html'); //sends user back to index upon completion of the form
@@ -70,15 +70,16 @@ app.post("/done", function (req, res) { //process form submission: req.body cont
 * Matching algorithm
 */
 var match = function(sub1, sub2){
-  var value = (sub1.cabort+sub2.cabort)*Math.abs(sub1.abort-sub2.abort)+
-        (sub1.cgov+sub2.cgov)*Math.abs(sub1.gov-sub2.gov)+
-        (sub1.cgun+sub2.cgun)*Math.abs(sub1.gun-sub2.gun)+
-        (sub1.cwarm+sub2.cwarm)*Math.abs(sub1.warm-sub2.warm)+
-        (sub1.cwage+sub2.cwage)*Math.abs(sub1.wage-sub2.wage)+
-        (sub1.cbord+sub2.cbord)*Math.abs(sub1.bord-sub2.bord)+
-        (sub1.clgbt+sub2.clgbt)*Math.abs(sub1.lgbt-sub2.lgbt)+
-        (sub1.cedu+sub2.cedu)*Math.abs(sub1.edu-sub2.edu)+
-        (sub1.cadmin+sub2.cadmin)*Math.abs(sub1.admin-sub2.admin);
+
+  var value = ((sub1.cabort=='on' | 0)+(sub2.cabort=='on' | 0))*Math.abs(sub1.abort-sub2.abort)+
+        ((sub1.cgov=='on' | 0)+(sub2.cgov =='on'| 0))*Math.abs(sub1.gov-sub2.gov)+
+        ((sub1.cgun=='on' | 0)+(sub2.cgun =='on'| 0))*Math.abs(sub1.gun-sub2.gun)+
+        ((sub1.cwarm =='on'|0)+(sub2.cwarm =='on'| 0))*Math.abs(sub1.warm-sub2.warm)+
+        ((sub1.cwage =='on'|0)+(sub2.cwage =='on'| 0))*Math.abs(sub1.wage-sub2.wage)+
+        ((sub1.cbord =='on'|0)+(sub2.cbord=='on' | 0))*Math.abs(sub1.bord-sub2.bord)+
+        ((sub1.clgbt =='on'|0)+(sub2.clgbt =='on'| 0))*Math.abs(sub1.lgbt-sub2.lgbt)+
+        ((sub1.cedu =='on'|0)+(sub2.cedu =='on'| 0))*Math.abs(sub1.edu-sub2.edu)+
+        ((sub1.cadmin =='on'|0)+(sub2.cadmin =='on'| 0))*Math.abs(sub1.admin-sub2.admin);
   return value/9;
 
 };
@@ -88,11 +89,11 @@ var pair = function(people){
   for(var i=0; i < people.length-1; i++){
     for(var a=i+1; a < people.length; a++){
       var val = match(people[i], people[a]);
-
-      if(val>55){
-        db.run ("UPDATE user_info SET paired = " + people[i].id + " WHERE ID = " + people[a].id + ";");
-        db.run ("UPDATE user_info SET paired = " + people[a].id + " WHERE ID = " + people[i].id + ";");
-        console.log("### "+people[i].first+" paired with "+people[a]".")
+      console.log(val);
+      if(val>50){
+        db.run ("UPDATE user_info SET paired = " + people[i].id + " WHERE id = " + people[a].id + ";");
+        db.run ("UPDATE user_info SET paired = " + people[a].id + " WHERE id = " + people[i].id + ";");
+        console.log("### "+people[i].first+" paired with "+people[a].first+".");
         people.splice(a,1);
       }
 
@@ -104,6 +105,10 @@ var pair = function(people){
 // TODO: check if this is even needed
 app.post("/form", function (req, res) { //when user is sent to form, send them form.html
     res.sendFile(__dirname + '/form.html');
+    db.all("SELECT * FROM user_info WHERE paired = 0", function (err, row){
+        var people = row;
+        console.log(row);
+      });
 });
 
 app.get('/', function(req, res){ //when someone connects initially, send the index
